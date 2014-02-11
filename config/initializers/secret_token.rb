026dcbbbc4ae7666d88c29f640d9a6999b914c95
YoonEricKim <jyk.ctw@gmail.com>
2014-02-10 17:49:40 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mentora::Application.config.secret_key_base = '3e440c99e0fd6d30b86b6c0ed2f652b40f7001324c2935318f419fdcfdd4d336c08fe1b7acdccd20c89235803348c5697a7a4dbef6ea4ff1ac1f933ee2535f1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mentora::Application.config.session_store :cookie_store, key: '_mentora_session'
