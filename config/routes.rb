842c40b3eb1f30f8945d043d0cff4f2c05f014eb
YoonEricKim <jyk.ctw@gmail.com>
2014-02-11 18:35:27 -0800

    Created home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Mentora::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about" #Creates about_path
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
