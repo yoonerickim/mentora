8ebf1988eefd1f58efd09362e1440190af7dfea0
YoonEricKim <jyk.ctw@gmail.com>
2014-03-14 03:01:27 -0700

    Update show view, icons, root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Mentora::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about" #Creates about_path
   
   # The priority is based upon order of creation: first created -> highest priority.
